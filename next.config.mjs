b361034515cdaa73e7a46515fb1d55f2a010a988
Miksio <mikolaj.loewnau@gmail.com>
2026-01-19 16:11:24 +0100

    Configure Next.js export and GitHub Pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: "/garmaz",
+  assetPrefix: "/garmaz/",
+  trailingSlash: true,
 }
 
 export default nextConfig
